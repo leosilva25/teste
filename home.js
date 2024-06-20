b1aecd33c527f049abddd6acdcb13e42414f3707
LEONARDO OLIVEIRA <114036441+leosilva25@users.noreply.github.com>
2024-06-20 01:19:59 -0300

    teste1

---
+++ home.js
@@ -0,0 +1,37 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.paragrafo-topo',{
+    duration: 2000,
+    distance:'90px',
+     origin:'left',
+     delay: 500
+})
+
+revelar.reveal('.img-topo',{
+    duration: 2000,
+    distance:'90px',
+    delay: 1000, 
+    origin: 'right'
+    
+})
+
+revelar.reveal('.foto1',{
+    duration: 2000,
+    distance:'90px',
+    delay: 800, 
+    origin:'left'
+})
+
+revelar.reveal('.foto2',{
+    duration: 2000,
+    distance:'90px',
+    delay: 900, 
+    origin:'bottom'
+})
+
+revelar.reveal('.foto3',{
+    duration: 2000,
+    distance:'90px',
+    delay: 1000, 
+    origin:'right'  
+})
